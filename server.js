f302046649b055bb4368968da97d0d0bd5f3aa66
SaurbhB <saurabh.hangingpanda@gmail.com>
2021-10-28 10:24:43 +0530

    post login

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+  
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoClient = require('mongoose');
+const {check, validationResult,loginValidation, } = require('express-validator')
+const bcrypt = require('bcrypt');
+const salt = 10
 
+//const _id =  ObjectId();
+//const {mongoClien, ObjectId } = require('mongodb')
+ const _id = require('mongodb')
 
 mongoClient.connect('mongodb://localhost:27017/Serverdb',{
 	useNewUrlParser:'true',
@@ -20,45 +27,142 @@ const userSchema = new mongoClient.Schema({
 	lastname : String,
 	username : String, 
 	email    : String, 
-	password : String, 
-	confirmpassword :String, 
+	password : String,
+  confirmpassword :String, 
 });
-const textUser = mongoClient.model('user',userSchema)
-module.exports = {textUser}
-console.log(typeof(textUser))
-app.use(bodyParser.urlencoded({extended:true}))
+
+
+userSchema.pre('save',function(next){
+    const user=this;
+    
+    if(user.isModified('password')){
+        bcrypt.genSalt(salt,function(err,salt){
+            if(err)return next(err);
+
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err);
+                user.password=hash;
+                user.confirmpassword=hash;
+                next();
+            })
+
+        })
+    }
+    else{
+        next();
+    }
+});
+
+userSchema.methods.comparepassword=function(password,cb){
+    bcrypt.compare(password,this.password,function(err,isMatch){
+        if(err) return cb(next);
+        cb(null,isMatch);
+    });
+}
+
+const textUser = mongoClient.model('textUser',userSchema)
+
+
+//console.log(textUser)
+
+app.use(bodyParser.urlencoded({extended:false}))
+
 
 app.get('/', (req, res)=>{
 	res.send('home page')
 })
 
-app.get('/user/register', (req, res)=>{
-	res.sendFile(__dirname + '/ServerIndex.html')
+
+app.post ('/user/registration',check("email","invalid email").isEmail(),
+  check("password").isLength({ min:5}),
+  check('confirmpassword').custom((value, { req }) => {
+    if (req.body.confirmpassword !== req.body.password) {
+      throw new Error('password must be same');
+    };
+    return true;
+  }),
+  (req, res) =>{
+  const firstamne = req.body.firstname;
+  const lastname = req.body.lastname;
+  const username = req.body.username;
+  const password = req.body.password;
+  const confirmpassword = req.body.confirmpassword;
+  const email = req.body.email;
+  const name = req.body.username;
+  
+   
+  
+
+
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty() ){
+    return res.status(200).json({errors:errors.array() });
+  };
+  textUser.findOne({username:name},(err, example)=>{
+    if (err)
+        console.log(err);
+    if (example){
+        return res.status(200).json({errors:'already Exist' });
+        console.log("this has in it");
+    } else {
+        const Example = new textUser (req.body);
+        Example.save(); 
+        
+        res.send('/registration');
+    }
+  })
 });
-app.post('/registration', (req, res) =>{
-	console.log(req.body,"all at once")
-	console.log ("Username-",req.body.username)
-	console.log("Password-",req.body.password)
-	console.log("confirmpassword-",req.body.confirmpassword)
-	const a = [];
-	const b = [];
-	a.push(req.body.password)
-	b.push(req.body.confirmpassword)
-	
-	const add = () => {
-		if ( a[0] === b[0]){
-			console.log("done")
-			textUser.insertMany(req.body)
-		}	else {
-			console.log('Req pww error')
-		}
-	}
-	add();	
-	res.sendFile(__dirname + '/registration.html')
+
+// const lastname = 4
+
+
+app.post('/user/login', check('email','invalid email').isEmail(),
+check('password').isLength({min:5}) ,(req, res)=>{
+
+    const email  = req.body.email;
+    const password = req.body.password;
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty() ){
+        return res.status(400).json({errors:errors.array() });    
+    };
+    textUser.findOne({email:email},(err, map)=>{
+        console.log(map._id)
+
+        var token = map._id;
+
+
+        //console.log(map)   {to get whole about save object}
+        if(map){
+            res.send({
+                // 'Access token': id,
+                'access token': map._id,
+            })
+        } else {
+            return res.status(500).json({errors:'none'})
+        }
+    })
 });
+
+console.log(token)
+const validateToken = (req, res, next)=>{
+    console.log('what?')
+    
+    next()
+}
+
+app.get("/user/get", (req,res)=>{
+
+
+
+
+
+
+    res.send('/')
+})
 
 app.listen(3001,()=>{
 	console.log('listened')
-})
+});
 
-// to be continued ....
